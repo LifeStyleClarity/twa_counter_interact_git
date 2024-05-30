2896ac981fb55b458c35a8d81d1177f4bd437320
Eduardo-clarity-ai <140390846+LifeStyleClarity@users.noreply.github.com>
2024-05-30 21:40:29 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
 //onst manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
-const manifestUrl = 'https://github.com/LifeStyleClarity/twa_counter_interact_git/blob/main/public/tonconnect-manifest.json';
+const manifestUrl = 'https://LifeStyleClarity.github.io/twa_counter_interact_git/tonconnect-manifest.json';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
